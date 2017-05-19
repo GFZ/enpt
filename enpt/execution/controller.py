33fb303a9d02ff64853f31550560da0a284ba553
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-05-19 18:05:12 +0200

    Bugfix

--- enpt/execution/controller.py
+++ enpt/execution/controller.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from ..model.images import EnMAP_L1B
 from ..processors.radiometric_transform import TOARad2TOARef_Transformer
 from GeoMultiSens.processing.process_controller import process_controller
 
@@ -9,14 +10,16 @@ from GeoMultiSens.processing.process_controller import process_controller
 class EnPT_controller(process_controller):
 
     def __init__(self, entity_ID):
-        # type: (str) -> self
+        # type: (str) -> None
 
         """EnPT Process Controller
 
         :param entity_ID:   ID of the scene to be processed, e.g. 'EM_20170515_12345'
         """
 
-        pass
+        raise NotImplementedError('The process controller is not yet working.')
+
+        self.Images = [EnMAP_L1B, ]
 
 
     def run_all_processors(self):
@@ -34,10 +37,10 @@ class EnPT_controller(process_controller):
         """
 
         # get a new instance of radiometric transformer
-        RT = TOARad2TOARef_Transformer()
+        RT = TOARad2TOARef_Transformer(None, None)
 
         # run transformation to TOARef
-        RT.transform()
+        RT.transform(self.Images[0])
 
 
     def run_atmospheric_correction(self):
