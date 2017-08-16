c2b8de95515327ce32992d9a80776418d3a749b7
André Hollstein <andre.hollstein@gfz-potsdam.de>
2017-08-16 13:59:50 +0200

    Make full coverage of L1B reader.

--- enpt/io/__init__.py
+++ enpt/io/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """EnPT Input / Output module."""
 
-from .EnMAPL1BProduct import EnMAPL1BProduct
+from .EnMAPL1BProduct import EnMAPL1BProduct, NoMatchError, MultipleMatchError
 
-_all__ = [EnMAPL1BProduct]
+_all__ = [EnMAPL1BProduct, NoMatchError, MultipleMatchError]
--- tests/test_enpt.py
+++ tests/test_enpt.py
@@ -25,7 +25,7 @@ class TestEnPT(unittest.TestCase):
         pass
 
     def test_IO_EnMAPL1BProduct(self):
-        from enpt.io import EnMAPL1BProduct
+        from enpt.io import EnMAPL1BProduct, NoMatchError, MultipleMatchError
         print("Test reading EnMAP Level-1B products")
         for l1b_file in glob(path.join(path.dirname(__file__), "data", "EnMAP_Level_1B", "*.zip")):
             print("File: %s" % l1b_file)
@@ -35,7 +35,18 @@ class TestEnPT(unittest.TestCase):
                     zf.extractall(tmpdir)
                     l1b_header_fn = glob(path.join(tmpdir, "*", "*_header.xml"))[0]
                     l1b = EnMAPL1BProduct(l1b_header_fn)
+                    print(l1b)
                     print(dir(l1b))
+                    node = l1b.getHeaderNode("detector1/centre_wavelength")
+                    wavelength = [float(v) for v in node.text.split(' ') if v.strip() != '']
+                    print(wavelength)
+
+                    node = l1b.getHeaderNode("detector2/centre_wavelength")
+                    wavelength = [float(v) for v in node.text.split(' ') if v.strip() != '']
+                    print(wavelength)
+
+                    self.assertRaises(NoMatchError, l1b.getHeaderNode, "klaus")
+                    self.assertRaises(MultipleMatchError, l1b.getHeaderNode, "*/centre_wavelength")
 
 
 if __name__ == "__main__":
