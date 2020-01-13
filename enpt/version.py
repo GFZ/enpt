bf3fbf06b04f9bd87edf71dc8fe8cc496a719cdb
Daniel Scheffler <danschef@gfz-potsdam.de>
2020-01-13 19:07:45 +0100

    Updated version info.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- enpt/version.py
+++ enpt/version.py
@@ -24,6 +24,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.9.11'
-__versionalias__ = '20200113.03'
+__version__ = '0.9.12'
+__versionalias__ = '20200113.04'
 __author__ = 'Daniel Scheffler'
