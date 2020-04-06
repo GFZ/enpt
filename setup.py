25b58964873eed5de1f32af15011f66a7db1875e
Daniel Scheffler <danschef@gfz-potsdam.de>
2020-04-06 17:32:06 +0200

    Added SICOR to setup.py requirements. Updated installation instructions. Revised test_enpt_install.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ with open("enpt/version.py", encoding='utf-8') as version_file:
 
 requirements = [  # put package requirements here
     'numpy', 'pandas', 'scipy', 'geoarray>=0.8.11', 'py_tools_ds>=0.14.23', 'arosics>=0.9.2', 'sensormapgeo>=0.2.1',
-    'cerberus', 'jsmin', 'matplotlib', 'tqdm', 'utm', 'lxml', 'numpy-indexed', 'mvgavg'
-    # 'sicor', # pip install git+https://gitext.gfz-potsdam.de/EnMAP/sicor.git
+    'cerberus', 'jsmin', 'matplotlib', 'tqdm', 'utm', 'lxml', 'numpy-indexed', 'mvgavg',
+    'sicor @ git+https://gitext.gfz-potsdam.de/EnMAP/sicor.git#egg=sicor'
 ]
 
 setup_requirements = ['setuptools-git']  # needed for package_data version controlled by GIT
