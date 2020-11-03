e44e41341092ee6be9b9bdc02d5fdabd13d628de
Daniel Scheffler <danschef@gfz-potsdam.de>
2020-11-03 20:10:28 +0100

    Fixed DeprecationWarning (Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated since Python 3.3, and in 3.9 it will stop working). Updated version info.

--- enpt/version.py
+++ enpt/version.py
@@ -27,6 +27,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.16.2'
-__versionalias__ = '20201012.03'
+__version__ = '0.16.3'
+__versionalias__ = '20201103.01'
 __author__ = 'Daniel Scheffler'
