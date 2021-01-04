75dd4304b954045ea7a690489925b4726bd98bc4
Daniel Scheffler <danschef@gfz-potsdam.de>
2021-01-04 16:57:08 +0100

    Updated URLs due to changes on the server side.

--- tests/gitlab_CI_docker/build_enpt_testsuite_image.sh
+++ tests/gitlab_CI_docker/build_enpt_testsuite_image.sh
@@ -7,8 +7,8 @@ gitlab_runner="enpt_gitlab_CI_runner"
 
 # get sicor project
 rm -rf context/sicor
-git clone git@gitext.gfz-potsdam.de:EnMAP/sicor.git ./context/sicor
-# git clone git@gitext.gfz-potsdam.de:EnMAP/sicor.git --branch feature/improve_enmap --single-branch ./context/sicor
+git clone git@git.gfz-potsdam.de:EnMAP/sicor.git ./context/sicor
+# git clone git@git.gfz-potsdam.de:EnMAP/sicor.git --branch feature/improve_enmap --single-branch ./context/sicor
 cd ./context/sicor
 git lfs pull
 cd ../..
