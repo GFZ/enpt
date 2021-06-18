4e6ee1e748a014bdeef0dd40bc93236f6e4871af
Daniel Scheffler <danschef@gfz-potsdam.de>
2021-06-19 00:08:25 +0200

    Bumped version.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- enpt/version.py
+++ enpt/version.py
@@ -27,6 +27,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.18.2'
-__versionalias__ = '20210617.01'
+__version__ = '0.18.3'
+__versionalias__ = '20210618.01'
 __author__ = 'Daniel Scheffler'
