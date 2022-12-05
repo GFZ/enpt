af671457cf7daa8c56b48652f778391ca5e40bb7
Daniel Scheffler <danschef@gfz-potsdam.de>
2022-12-05 18:13:24 +0100

    Fix code style issues.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- enpt/processors/atmospheric_correction/atmospheric_correction.py
+++ enpt/processors/atmospheric_correction/atmospheric_correction.py
@@ -196,12 +196,12 @@ class AtmosphericCorrector(object):
                                   wl_ref_swir_water,
                                   boa_ref_swir_land)
         waterboa_additional_results = np.where((enmap_ImageL1.vnir.mask_landwater[:] == 2)[:, :, None],
-                                  water_additional_results,
-                                  boa_ref_vnir_land)
-        #from geoarray import GeoArray
-        #GeoArray(waterboa_additional_results[1]).save(
-        #    '/home/alvarado/repositories/acwater/tests/data/output/EnPT_Chla_new.tif', fmt='GTiff')
-        #data_geoarray = waterboa_additional_results
+                                               water_additional_results,
+                                               boa_ref_vnir_land)
+        # from geoarray import GeoArray
+        # GeoArray(waterboa_additional_results[1]).save(
+        #     '/home/alvarado/repositories/acwater/tests/data/output/EnPT_Chla_new.tif', fmt='GTiff')
+        # data_geoarray = waterboa_additional_results
 
         return wlboa_ref_vnir, wlboa_ref_swir, waterboa_additional_results, land_additional_results
 
