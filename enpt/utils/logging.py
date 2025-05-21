6b0b1b06088c8ca1e83c074d22710e4ce5ba27b4
Daniel Scheffler <danschef@gfz.de>
2025-05-21 18:51:13 +0200

    Typo
    Signed-off-by: Daniel Scheffler <danschef@gfz.de>

--- enpt/utils/logging.py
+++ enpt/utils/logging.py
@@ -99,7 +99,7 @@ class EnPT_Logger(logging.Logger):
         streamHandler.set_name('StringIO handler')
 
         # create ConsoleHandler for logging levels DEGUG and INFO -> logging to sys.stdout
-        consoleHandler_out = logging.StreamHandler(stream=sys.stdout)  # by default it would go to sys.stderr
+        consoleHandler_out = logging.StreamHandler(stream=sys.stdout)  # by default, it would go to sys.stderr
         consoleHandler_out.setFormatter(self.formatter_ConsoleH)
         consoleHandler_out.set_name('console handler stdout')
         consoleHandler_out.setLevel(log_level)
