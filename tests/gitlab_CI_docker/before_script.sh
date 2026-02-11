1328b2b583ae64d2d9f467cd8f21eaf43576a270
Daniel Scheffler <danschef@gfz.de>
2026-02-11 17:13:50 +0100

    Building a SICOR wheel installing isofit@dev is currently not needed.
    Signed-off-by: Daniel Scheffler <danschef@gfz.de>

--- tests/gitlab_CI_docker/before_script.sh
+++ tests/gitlab_CI_docker/before_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # get sicor project
-rm -rf context/sicor
-git clone git@git.gfz.de:EnMAP/sicor.git ./context/sicor
-# git clone git@git.gfz.de:EnMAP/sicor.git --branch feature/improve_enmap --single-branch ./context/sicor
-cd ./context/sicor || exit
-git lfs pull
-pip wheel --no-deps -w dist .
-cd ../..
+# rm -rf context/sicor
+# git clone git@git.gfz.de:EnMAP/sicor.git ./context/sicor
+# # git clone git@git.gfz.de:EnMAP/sicor.git --branch feature/improve_enmap --single-branch ./context/sicor
+# cd ./context/sicor || exit
+# git lfs pull
+# pip wheel --no-deps -w dist .
+# cd ../..
