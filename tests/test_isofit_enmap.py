9453b17799fca9fd5457f37514cad8297e441769
Daniel Scheffler <danschef@gfz.de>
2026-03-05 20:13:30 +0100

    Config should be initialized from config_for_testing_dlr.
    Signed-off-by: Daniel Scheffler <danschef@gfz.de>

--- tests/test_isofit_enmap.py
+++ tests/test_isofit_enmap.py
@@ -55,7 +55,7 @@ from geoarray import GeoArray
 
 from enpt.io.reader import L1B_Reader
 from enpt.processors.orthorectification import Orthorectifier
-from enpt.options.config import EnPTConfig, config_for_testing, config_for_testing_dlr
+from enpt.options.config import EnPTConfig, config_for_testing_dlr
 from enpt.processors.atmospheric_correction._isofit_downloads import download_isofit_resources
 from enpt.processors.atmospheric_correction._isofit_enmap import IsofitEnMAP
 from enpt.processors.atmospheric_correction._isofit_lut_preparation import LUTTransformer
@@ -70,7 +70,7 @@ class Test_ISOFIT_EnMAP(unittest.TestCase):
     """Tests for ISOFIT_EnMAP class."""
 
     def setUp(self):
-        self.config = EnPTConfig(**config_for_testing)
+        self.config = EnPTConfig(**config_for_testing_dlr)
 
         self.tmpdir = tempfile.mkdtemp(dir=self.config.working_dir)
 
