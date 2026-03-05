d82717eab2c06d6b24cde61b26454e5aa2cc623c
Daniel Scheffler <danschef@gfz.de>
2026-03-05 20:00:47 +0100

    Drop integration test based on deprecated dummy EnMAP format.
    Signed-off-by: Daniel Scheffler <danschef@gfz.de>

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -42,21 +42,9 @@ import os
 import pytest
 
 from enpt.execution.controller import EnPT_Controller
-from enpt.options.config import config_for_testing, config_for_testing_dlr, config_for_testing_water
+from enpt.options.config import config_for_testing_dlr, config_for_testing_water
 
 __author__ = 'Daniel Scheffler'
-
-
-class Test_EnPT_Controller(TestCase):
-    def setUp(self):
-        self.CTR = EnPT_Controller(**config_for_testing)
-
-    def tearDown(self):
-        # NOTE: ignore_errors deletes the folder, regardless of whether it contains read-only files
-        shutil.rmtree(self.CTR.cfg.output_dir, ignore_errors=True)
-
-    def test_run_all_processors(self):
-        self.CTR.run_all_processors()
 
 
 class Test_EnPT_Controller_DLR_testdata(TestCase):
